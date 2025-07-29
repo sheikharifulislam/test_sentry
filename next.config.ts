241452a757aeb3d7f75ed954085e7a2471d9dc6b
sheikharifulislam <sheikharifulislam101@gmail.com>
2025-07-30 01:23:35 +0600

    feat: initial setup complete

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
+const isCI = process.env.CI === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    productionBrowserSourceMaps: isCI,
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+    // Only print logs for uploading source maps in CI
+    silent: !isCI,
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+    telemetry: false,
+
+    // Disable Sentry Webpack config in development mode.
+    disableSentryWebpackConfig: true,
+});
